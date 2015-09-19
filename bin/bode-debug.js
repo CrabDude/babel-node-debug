1a583ac651fda067b6faaa7463e2a4f2606161a1
Adam Crabtree <dude@noderiety.com>
2015-09-18 18:43:28 -0700

    Set breakpoint as close to main (not _babel-node) as possible

--- bin/bode-debug.js
+++ bin/bode-debug.js
@@ -1,6 +1,55 @@
 #!/usr/bin/env node
 
+var Debugger = require('yadc').Debugger
 var babelNodePath = require.resolve('babel/bin/babel-node')
+var path = require('path')
+var nodeDebugDir = path.dirname(require.resolve('node-inspector/bin/node-debug'))
 process.argv.splice(2, 0, '--nodejs', babelNodePath)
 
-require('node-inspector/bin/node-debug')()
+var oldFork = require('child_process').fork
+require('child_process').fork = function fork() {
+  var childProcess = oldFork.apply(this, arguments)
+  if (arguments[0].indexOf(nodeDebugDir) === -1) {
+    setTimeout(function() {
+      // var script = path.resolve(process.cwd(), config.subproc.script)
+      var debug = new Debugger({
+        port: 5858,
+        host: 'localhost'
+      })
+      debug.send({command: 'continue'}, function() {
+        debug.send({
+            command: 'setBreakpoint',
+            arguments: {
+              type: 'scriptRegExp',
+              target: config.subproc.script,
+              line: 1,
+              column: 1
+            }
+          }, function(err, result) {
+            if (err) return
+            debug.on('event', function listener(event) {
+              if (event.event === 'break') {
+                debug.send({
+                    command: 'clearbreakpoint',
+                    arguments: {
+                      type: 'scriptRegExp',
+                      breakpoint: result.res.body.breakpoint
+                    }
+                  })
+                debug.client.end()
+                debug.removeListener('event', listener)
+                debug.removeListener('error', onError)
+              }
+            })
+        })
+      })
+      function onError(errObj) {}
+      debug.on('error', onError)
+    }, 1000)
+  }
+  return childProcess
+}
+var main = require('node-inspector/bin/node-debug')
+require('child_process').fork = oldFork
+var config = main.createConfig(process.argv.slice(2))
+main()
