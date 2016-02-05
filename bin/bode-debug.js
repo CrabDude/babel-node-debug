aa9f54e80975b9bfa2c26dbf31d494b207f51507
Adam Crabtree <dude@noderiety.com>
2016-02-04 18:50:56 -0800

    Set breakpoint on file instead of pattern match

--- bin/bode-debug.js
+++ bin/bode-debug.js
@@ -1,54 +1,53 @@
 #!/usr/bin/env node
 
-var Debugger = require('yadc').Debugger
+var BabelDebugger = require('../lib/BabelDebugger')
 var babelNodePath = require.resolve('babel-cli/bin/babel-node')
 var path = require('path')
+var child_process = require('child_process')
+
+
 var nodeDebugDir = path.dirname(require.resolve('node-inspector/bin/node-debug'))
 process.argv.splice(2, 0, '--nodejs', babelNodePath)
 
-var oldFork = require('child_process').fork
-require('child_process').fork = function fork() {
+// Pass shimmed fork to node-debug
+var oldFork = child_process.fork
+child_process.fork = fork
+var main = require('node-inspector/bin/node-debug')
+child_process.fork = oldFork
+
+// Get config
+var config = main.createConfig(process.argv.slice(2))
+var filename = path.resolve(process.cwd(), config.subproc.script)
+
+// Start Debugger
+main()
+
+function fork() {
   var childProcess = oldFork.apply(this, arguments)
   if (arguments[0].indexOf(nodeDebugDir) === -1) {
     setTimeout(function() {
-      var debug = new Debugger({
+      var debug = new BabelDebugger({
         port: config.subproc.debugPort,
         host: '127.0.0.1'
       })
-      debug.send({command: 'continue'}, function() {
-        debug.send({
-            command: 'setBreakpoint',
-            arguments: {
-              type: 'scriptRegExp',
-              target: config.subproc.script,
-              line: 1,
-              column: 1
-            }
-          }, function(err, result) {
-            if (err) return
-            debug.on('event', function listener(event) {
-              if (event.event === 'break') {
-                debug.send({
-                    command: 'clearbreakpoint',
-                    arguments: {
-                      type: 'scriptRegExp',
-                      breakpoint: result.res.body.breakpoint
-                    }
-                  })
-                debug.client.end()
-                debug.removeListener('event', listener)
-                debug.removeListener('error', onError)
-              }
-            })
-        })
-      })
-      function onError(errObj) {}
+      function onError() {}
       debug.on('error', onError)
+      debug.on('event', function listener(event) {
+        if (event.event === 'afterCompile' && event.body.script.name === filename) {
+          debug.client.end()
+          debug.removeListener('event', listener)
+          debug.removeListener('error', onError)
+        }
+      })
+
+      debug.clearAll()
+        .then(function(result) {
+          return debug.setInitialBreakpoint(filename)
+        })
+        .then(function(result) {
+          return debug.sendContinue()
+        })
     }, 1000)
   }
   return childProcess
 }
-var main = require('node-inspector/bin/node-debug')
-require('child_process').fork = oldFork
-var config = main.createConfig(process.argv.slice(2))
-main()
